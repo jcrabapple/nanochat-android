f3d8a674cc3cd86dbd2d79e8c1ae0dc6db921472
Jason Crabtree <jason.crabtree@gmail.com>
2026-01-12 14:23:40 -0500

    chore: Bump version to 0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.nanogpt.chat"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 6
+        versionName = "0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
