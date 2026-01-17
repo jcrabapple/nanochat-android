a8c2c2aa72382b4be1e72edbb810d53aadb51f4d
Jason Crabtree <jason.crabtree@gmail.com>
2026-01-16 20:26:28 -0500

    Release v1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.nanogpt.chat"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "0.9.3"
+        versionCode = 21
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
